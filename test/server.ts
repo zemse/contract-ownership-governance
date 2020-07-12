160ed2fa190c1cc3a25f5e403aaea57b12237d0e
zemse <sohamzemse143@gmail.com>
2020-07-13 00:28:20 +0530

    Set default gas price as 0 in ganache

--- test/server.ts
+++ test/server.ts
@@ -1,7 +1,9 @@
 import ganache from 'ganache-core';
 
 export const startGanacheServer = () => {
-  global.server = ganache.server();
+  global.server = ganache.server({
+    gasPrice: '0',
+  });
   global.server.listen(7545, () => {
     console.log('\x1b[2m%s\x1b[0m', '      Ganache Started on 7545..');
   });
