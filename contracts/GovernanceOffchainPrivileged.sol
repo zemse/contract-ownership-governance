48a66c726e0964f7e41d9dfcdf242475937cf150
zemse <sohamzemse143@gmail.com>
2020-07-12 19:48:15 +0530

    Update contracts with latest interfaces

---
+++ contracts/GovernanceOffchainPrivileged.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.10;
+pragma experimental ABIEncoderV2;
+
+import "./lib/ECDSA.sol";
+import "./interfaces/IGovernanceOffchain.sol";
+import "./interfaces/IGovernancePrivileged.sol";
+
+/// @title Governance Offchain Smart Contract
+/// @notice Governs a decentralised application to perform administrative tasks.
+contract Governance is IGovernanceOffchain, IGovernancePrivileged {
+    /// @dev EIP-191 Prepend byte + Version byte
+    bytes public PREFIX = abi.encodePacked((hex"1900"), address(this));
+
+    /// @dev Prevents replay of transactions. It is used as nonce.
+    uint256 public override transactionsCount;
+
+    /// @dev Sum of the powers of all governors
+    uint256 public override totalPower;
+
+    uint256[2] public required;
+
+    /// @dev Governor addresses with corresponding powers (vote weightage)
+    mapping(address => uint256) powers;
+
+    modifier onlyGovernance() {
+        require(msg.sender == address(this), "Only Governance");
+        _;
+    }
+
+    /// @notice Stores initial set of governors
+    /// @param _governors List of initial governor addresses
+    /// @param _powers List of corresponding initial powers
+    constructor(address[] memory _governors, uint256[] memory _powers) public {
+        require(_governors.length == _powers.length, "Gov: Invalid input lengths");
+
+        uint256 _totalPower;
+        for (uint256 i = 0; i < _governors.length; i++) {
+            powers[_governors[i]] = _powers[i];
+            _totalPower += _powers[i];
+            emit GovernorPowerUpdated(_governors[i], _powers[i]);
+        }
+        totalPower = _totalPower;
+    }
+
+    /// @notice Calls the dApp to perform administrative task
+    /// @param _nonce Serial number of transaction
+    /// @param _destination Address of contract to make a call to, should be dApp address
+    /// @param _data Input data in the transaction
+    /// @param _signatures Signatures of governors collected off chain
+    /// @dev The signatures are required to be sorted to prevent duplicates
+    function executeTransaction(
+        uint256 _nonce,
+        address _destination,
+        bytes memory _data,
+        bytes[] memory _signatures
+    ) external override payable {
+        require(_nonce >= transactionsCount, "Gov: Nonce is already used");
+        require(_nonce == transactionsCount, "Gov: Nonce is too high");
+
+        bytes32 _digest = keccak256(abi.encodePacked(PREFIX, _nonce, _destination, _data));
+
+        verifySignatures(_digest, _signatures);
+
+        transactionsCount++;
+
+        (bool _success, ) = _destination.call{ value: msg.value }(_data);
+        require(_success, "Call was reverted");
+    }
+
+    /// @notice Updates governor statuses
+    /// @param _governor List of governor addresses
+    /// @param _newPrivilege List of corresponding new powers
+    function updatePower(address _governor, uint256 _newPrivilege)
+        external
+        override
+        onlyGovernance
+    {
+        require(msg.sender == address(this), "Gov: Only self can call");
+
+        uint256 _totalPower = totalPower;
+
+        if (_newPrivilege != powers[_governor]) {
+            // TODO: Add safe math
+            _totalPower = _totalPower - powers[_governor] + _newPrivilege;
+
+            powers[_governor] = _newPrivilege;
+        }
+
+        totalPower = _totalPower;
+
+        emit GovernorPowerUpdated(_governor, _newPrivilege);
+    }
+
+    /// @notice Gets the consensus privilege of the governor
+    /// @param _governor Address of the governor
+    /// @return The governor's voting powers
+    function powerOf(address _governor) public override view returns (uint256) {
+        return powers[_governor];
+    }
+
+    /// @notice Checks for consensus
+    /// @param _digest hash of sign data
+    /// @param _signatures sorted sigs according to increasing signer addresses
+    function verifySignatures(bytes32 _digest, bytes[] memory _signatures) internal view {
+        uint160 _lastGovernor;
+        uint256 _consensus;
+        for (uint256 i = 0; i < _signatures.length; i++) {
+            address _signer = ECDSA.recover(_digest, _signatures[i]);
+
+            // Prevents duplicate signatures
+            uint160 _thisGovernor = uint160(_signer);
+            require(_thisGovernor > _lastGovernor, "Gov: Invalid arrangement");
+            _lastGovernor = _thisGovernor;
+
+            require(powerOf(_signer) > 0, "Gov: Not a governor");
+            _consensus += powerOf(_signer);
+        }
+
+        // 66% consensus
+        // TODO: Add safe math
+        require(_consensus * 3 > totalPower * 2, "Gov: Not 66% consensus");
+    }
+
+    function getRequiredVotes() public override view returns (uint256) {
+        return (required[0] * totalPower) / required[1];
+    }
+
+    function setRequiredVotes(uint256 _numerator, uint256 _denominator)
+        public
+        override
+        onlyGovernance
+    {
+        required[0] = _numerator;
+        required[1] = _denominator;
+    }
+}
--- contracts/GovernanceOnchainEqual.sol
+++ contracts/GovernanceOnchainEqual.sol
@@ -3,36 +3,27 @@
 pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
-import "./IGovernanceOnchain.sol";
-import "./IWeightedGovernance.sol";
+import "./interfaces/IGovernanceOnchain.sol";
+import "./interfaces/IGovernanceEqual.sol";
 
-contract Governance is GovernanceOnchain, WeightedGovernance {
+contract Governance is IGovernanceOnchain, IGovernanceEqual {
     /// @dev Transactions proposed for being executed
     Transaction[] transactions;
 
-    /// @dev Sum of the privileges of all governors
-    uint256 public override totalPrivileges;
+    uint256[2] public required;
+
+    address[] governors;
 
     /// @dev Transaction confirmation given by individual governors
     mapping(uint256 => mapping(address => bool)) confirmation;
 
-    /// @dev Governor addresses with corresponding privileges (vote weightage)
-    mapping(address => uint256) privileges;
-
     /// @notice Stores initial set of governors
     /// @param _governors List of initial governor addresses
-    /// @param _privileges List of corresponding initial privileges
-    constructor(address[] memory _governors, uint256[] memory _privileges) public {
-        require(_governors.length == _privileges.length, "Gov: Invalid input lengths");
-
-        uint256 _totalPrivileges;
+    constructor(address[] memory _governors) public {
+        governors = _governors;
         for (uint256 i = 0; i < _governors.length; i++) {
-            privileges[_governors[i]] = _privileges[i];
-            _totalPrivileges += _privileges[i];
+            emit GovernorAdded(_governors[i]);
         }
-        totalPrivileges = _totalPrivileges;
-
-        emit GovernorsPrivilegeUpdated(_governors, _privileges);
     }
 
     /// @dev Allows an governor to submit and confirm a transaction.
@@ -40,7 +31,7 @@ contract Governance is GovernanceOnchain, WeightedGovernance {
     /// @param _value Transaction ether value.
     /// @param _data Transaction data payload.
     /// @return Returns transaction ID.
-    function submitTransaction(
+    function createTransaction(
         address _destination,
         uint256 _value,
         bytes memory _data
@@ -52,7 +43,7 @@ contract Governance is GovernanceOnchain, WeightedGovernance {
                 value: _value,
                 data: _data,
                 executed: false,
-                consensus: 0
+                votes: 0
             })
         );
 
@@ -65,25 +56,23 @@ contract Governance is GovernanceOnchain, WeightedGovernance {
     /// @dev Allows a governor to confirm a transaction.
     /// @param _transactionId Transaction ID.
     function confirmTransaction(uint256 _transactionId) public override {
-        require(privileges[msg.sender] > 0, "Gov: Only governors can call");
         require(_transactionId < transactions.length, "Gov: Tx doesnt exists");
         require(!transactions[_transactionId].executed, "Gov: Tx already executed");
         require(confirmation[_transactionId][msg.sender], "Gov: Already confirmed");
 
         confirmation[_transactionId][msg.sender] = true;
-        transactions[_transactionId].consensus += getGovernorPrivileges(msg.sender);
+        transactions[_transactionId].votes += 1;
     }
 
     /// @dev Allows a governor to revoke a confirmation for a transaction.
     /// @param _transactionId Transaction ID.
     function revokeConfirmation(uint256 _transactionId) public override {
-        require(privileges[msg.sender] > 0, "Gov: Only governors can call");
         require(_transactionId < transactions.length, "Gov: Tx doesnt exists");
         require(!transactions[_transactionId].executed, "Gov: Tx already executed");
         require(!confirmation[_transactionId][msg.sender], "Gov: Not confirmed");
 
         confirmation[_transactionId][msg.sender] = false;
-        transactions[_transactionId].consensus -= getGovernorPrivileges(msg.sender);
+        transactions[_transactionId].votes -= 1;
     }
 
     function executeTransaction(uint256 _transactionId) public override {
@@ -97,40 +86,7 @@ contract Governance is GovernanceOnchain, WeightedGovernance {
     }
 
     function isTransactionConfirmed(uint256 _transactionId) internal view returns (bool) {
-        return transactions[_transactionId].consensus * 3 > totalPrivileges * 2;
-    }
-
-    /// @notice Updates governor statuses
-    /// @param _governors List of governor addresses
-    /// @param _newPrivileges List of corresponding new privileges
-    function updatePrivileges(address[] memory _governors, uint256[] memory _newPrivileges)
-        external
-        override
-    {
-        require(msg.sender == address(this), "Gov: Only self can call");
-        require(_governors.length == _newPrivileges.length, "Gov: Invalid input lengths");
-
-        uint256 _totalPrivileges = totalPrivileges;
-
-        for (uint256 i = 0; i < _governors.length; i++) {
-            if (_newPrivileges[i] != privileges[_governors[i]]) {
-                // TODO: Add safe math
-                _totalPrivileges = _totalPrivileges - privileges[_governors[i]] + _newPrivileges[i];
-
-                privileges[_governors[i]] = _newPrivileges[i];
-            }
-        }
-
-        totalPrivileges = _totalPrivileges;
-
-        emit GovernorsPrivilegeUpdated(_governors, _newPrivileges);
-    }
-
-    /// @notice Gets the consensus privilege of the governor
-    /// @param _governor Address of the governor
-    /// @return The governor's voting privileges
-    function getGovernorPrivileges(address _governor) public override view returns (uint256) {
-        return privileges[_governor];
+        return transactions[_transactionId].votes >= getRequiredVotes();
     }
 
     function getTransaction(uint256 _transactionId)
@@ -140,5 +96,67 @@ contract Governance is GovernanceOnchain, WeightedGovernance {
         returns (Transaction memory)
     {
         return transactions[_transactionId];
+    }
+
+    function getRequiredVotes() public override view returns (uint256) {
+        return (required[0] * governors.length) / required[1];
+    }
+
+    function setRequiredVotes(uint256 _numerator, uint256 _denominator) public override {
+        required[0] = _numerator;
+        required[1] = _denominator;
+    }
+
+    function getGovernors() public override view returns (address[] memory) {
+        return governors;
+    }
+
+    function addGovernor(address _newGovernor) public override {
+        require(!isGovernor(_newGovernor), "Already a governor");
+        governors.push(_newGovernor);
+    }
+
+    function governorsCount() public override view returns (uint256) {
+        return governors.length;
+    }
+
+    function isGovernor(address _governor) public override view returns (bool) {
+        for (uint256 i = 0; i < governors.length; i++) {
+            if (governors[i] == _governor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function removeGovernor(address _existingGovernor) public override {
+        uint256 _index;
+        bool _exists;
+        for (uint256 i = 0; i < governors.length; i++) {
+            if (governors[i] == _existingGovernor) {
+                _index = i;
+                _exists = true;
+            }
+        }
+
+        require(_exists, "Governor does not exist");
+
+        governors[_index] = governors[governors.length - 1];
+        governors.pop();
+    }
+
+    function replaceGovernor(address _governor, address _newGovernor) public override {
+        uint256 _index;
+        bool _exists;
+        for (uint256 i = 0; i < governors.length; i++) {
+            if (governors[i] == _governor) {
+                _index = i;
+                _exists = true;
+            }
+        }
+
+        require(_exists, "Governor does not exist");
+
+        governors[_index] = _newGovernor;
     }
 }
